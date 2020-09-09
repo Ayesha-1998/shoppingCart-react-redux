e7c26442c191ac8b0bc179983650acde68dca50d
Ayesha <ayesha.cjc@gmail.com>
2020-09-09 18:20:40 +0530

    Updates

---
+++ precache-manifest.be494fa1a5b572ccd274e5d914c694fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5587dd3934e1f01810cd5c83b768be",
+    "url": "/shoppingCart-react-redux/index.html"
+  },
+  {
+    "revision": "ab8a436dc9221f403f34",
+    "url": "/shoppingCart-react-redux/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ab8a436dc9221f403f34",
+    "url": "/shoppingCart-react-redux/static/js/2.1c687ceb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingCart-react-redux/static/js/2.1c687ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde63e69b32b5aa2698f",
+    "url": "/shoppingCart-react-redux/static/js/main.547dfca1.chunk.js"
+  },
+  {
+    "revision": "7d1c7786d7a7a62abe36",
+    "url": "/shoppingCart-react-redux/static/js/runtime-main.0744d6ae.js"
+  }
+]);
